1bfa37d542d4316a827d8ad3fbf77ba4d315b566
siikamiika <siikamiika@users.noreply.github.com>
2021-11-18 19:09:15 +0200

    ssh improve private key parsing

--- ssh/analyze_private_key.py
+++ ssh/analyze_private_key.py
@@ -31,82 +31,151 @@ def read_ssh_private_key_bytes(filename):
     return base64.b64decode(b''.join(data_lines))
 
 def decode_ssh_private_key_bytes(data):
-    Bignum = Struct(
-        'size' / Int32ub,
-        'value' / BytesInteger(lambda this: this.size)
-    )
-
-    SSHPubkey = Struct(
-        'key_name' / PascalString(Int32ub, 'ascii'),
-        'key' / Switch(this.key_name, {
-            'ssh-rsa': Struct(
-                'e' / Bignum,
-                'n' / Bignum,
-            ),
-            'ecdsa-sha2-nistp256': Struct(
-                # TODO
-            ),
-            'ssh-ed25519': Struct(
-                # TODO
-            ),
-            'ssh-dss': Struct(
-                # TODO
-            )
-        }),
-        # 'rest' / GreedyBytes
-    )
-
     # https://cvsweb.openbsd.org/src/usr.bin/ssh/PROTOCOL.key?annotate=HEAD
     # https://datatracker.ietf.org/doc/html/draft-miller-ssh-agent-04
+    # https://dnaeon.github.io/openssh-private-key-binary-format/
 
-    SSHPrivkey = Struct(
-        'magic' / NullTerminated(Const(b'openssh-key-v1')),
+    # definitions
+
+    # https://github.com/openssh/openssh-portable/blob/d902d728dfd81622454260e23bc09d5e5a9a795e/cipher.c#L67-L113
+    cipher_block_sizes = {
+        'none': 8,
+        'aes256-ctr': 16,
+    }
+
+    # common formats
+
+    MPInt = Struct(
+        'size' / Int32ub,
+        'value' / BytesInteger(this.size)
+    )
+
+    # private key encryption options format
+
+    SSHPrivkeyCryptOptions = Struct(
         'ciphername' / PascalString(Int32ub, 'ascii'),
         'kdfname' / PascalString(Int32ub, 'ascii'),
         'kdfoptions' / Struct(
             'size' / Int32ub,
             'value' / Bytes(this.size) # TODO parse
         ),
-        'num_pubkeys' / Int32ub,
-        'pubkeys' / Array(
-            this.num_pubkeys,
-            Struct(
-                'size' / Int32ub,
-                'pubkey' / FixedSized(this.size, SSHPubkey)
-            )
-        ),
-        'privkeys' / Struct(
-            'size' / Int32ub,
-            'value' / FixedSized(
-                this.size,
-                # _ means parent
-                Switch(this._.ciphername, {
-                    'none': Struct(
-                        # TODO assert checkint1 == checkint2
-                        'checkint1' / Int32ub,
-                        'checkint2' / Int32ub,
-                        # TODO support multiple private keys
-                        # is it the same amount as pubkeys?
-                        'privkey_magic_size' / Int32ub,
-                        'privkey_magic' / FixedSized(this.privkey_magic_size, GreedyBytes),
-                        'n' / Bignum,
-                        'e' / Bignum,
-                        'd' / Bignum,
-                        'iqmp' / Bignum,
-                        'p' / Bignum,
-                        'q' / Bignum,
-                        'comment' / PascalString(Int32ub, 'ascii'),
-                        # TODO not so greedy
-                        'pad' / GreedyBytes,
-                    ),
-                    'aes256-ctr': GreedyBytes
-                }),
-            )
-        ),
+    )
+
+    # embedded public key format
+
+    SSHRSAPubkey = Struct(
+        'e' / MPInt,
+        'n' / MPInt,
+    )
+
+    SSHECDSAPubkey = Struct(
+        # TODO
+    )
+
+    SSHed25519Pubkey = Struct(
+        # TODO
+    )
+
+    SSHDSSPubkey = Struct(
+        # TODO
+    )
+
+    SSHPubkeyFile = Struct(
+        'type' / PascalString(Int32ub, 'ascii'),
+        'key' / Switch(this.type, {
+            'ssh-rsa': SSHRSAPubkey,
+            'ecdsa-sha2-nistp256': SSHECDSAPubkey,
+            'ssh-ed25519': SSHed25519Pubkey,
+            'ssh-dss': SSHDSSPubkey,
+        }),
         # 'rest' / GreedyBytes
     )
 
-    print(SSHPrivkey.parse(data))
+    SSHEmbeddedPubkeys = Struct(
+        'size' / Int32ub,
+        'values' / Array(
+            this.size,
+            Struct(
+                'size' / Int32ub,
+                'value' / FixedSized(this.size, SSHPubkeyFile)
+            )
+        ),
+    )
+
+    # private key format
+
+    SSHRSAPrivkey = Struct(
+        'n' / MPInt,
+        'e' / MPInt,
+        'd' / MPInt,
+        'iqmp' / MPInt,
+        'p' / MPInt,
+        'q' / MPInt,
+        'comment' / PascalString(Int32ub, 'ascii'),
+        'pad' / Bytes(
+            lambda this: (
+                len(this._.type) + 4
+                + sum(this[x].size for x in ['n', 'e', 'd', 'iqmp', 'p', 'q'])
+                + len(this.comment) + 4
+            ) % cipher_block_sizes[this._._._._.cryptoptions.ciphername]
+        ),
+    )
+
+    SSHECDSAPrivkey = Struct(
+        # TODO
+    )
+
+    SSHed25519Privkey = Struct(
+        # TODO
+    )
+
+    SSHDSSPrivkey = Struct(
+        # TODO
+    )
+
+    SSHPrivkey = Struct(
+        'type' / PascalString(Int32ub, 'ascii'),
+        'key' / Switch(this.type, {
+            'ssh-rsa': SSHRSAPrivkey,
+            'ecdsa-sha2-nistp256': SSHECDSAPrivkey,
+            'ssh-ed25519': SSHed25519Privkey,
+            'ssh-dss': SSHDSSPrivkey,
+        }),
+    )
+
+    SSHPrivkeysUnencrypted = Struct(
+        # TODO assert checkint1 == checkint2
+        'checkint1' / Int32ub,
+        'checkint2' / Int32ub,
+        'privkeys' / Array(
+            this._._.pubkeys.size,
+            SSHPrivkey
+        ),
+    )
+
+    SSHPrivkeys = Struct(
+        'size' / Int32ub,
+        'value' / FixedSized(
+            this.size,
+            # _ means parent
+            Switch(this._.cryptoptions.ciphername, {
+                'none': SSHPrivkeysUnencrypted,
+                'aes256-ctr': GreedyBytes
+            }),
+        )
+    )
+
+    # overall private key file format
+
+    SSHPrivkeyFile = Struct(
+        'magic' / NullTerminated(Const(b'openssh-key-v1')),
+        'cryptoptions' / SSHPrivkeyCryptOptions,
+        'pubkeys' / SSHEmbeddedPubkeys,
+        'privkeys' / SSHPrivkeys,
+        # 'rest' / GreedyBytes
+    )
+
+    print(SSHPrivkeyFile.parse(data).privkeys)
 
 def write_stdout_bytes(data):
     with os.fdopen(sys.stdout.fileno(), 'wb', closefd=False) as stdout:
